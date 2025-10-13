ef3541fdfa7308ee57b418fde850f6153c58fe23
unknown <marcio_82248@ETEC.FURLAN>
2025-10-13 20:13:58 -0300

    novo codigo

--- src/app/api/login/route.js
+++ src/app/api/login/route.js
@@ -1,20 +1,27 @@
 import { NextResponse, NextRequest } from "next/server";
-                    import jwt from "jsonwebtoken";
-                    const JWT_SECRET = 'Minha_cH@ve_t&st&@2025';
-                    const mockUsers = [
-                        {
-                            id: 1,
-                            username: "Marcio Cezar",
-                            login: "marcio.cezar",
-                            password: "secret@123"
-                        },
-                        {
-                            id: 2,
-                            username: "Rogerio Cezar",
-                            login: "rogerio.cezar",
-                            password: "secret@123"
-                        }
-                    ];
+import jwt from "jsonwebtoken";
+
+const JWT_SECRET = 'Minha_cH@ve_t&st&@2025';
+const mockUsers = [
+    {
+        id: 1,
+        username: "Marcio Cezar",
+        login: "marcio.cezar",
+        password: "secret@123"
+    },
+    {
+        id: 2,
+        username: "Rogerio Cezar",
+        login: "rogerio.cezar",
+        password: "secret@123"
+    },
+    {
+        id: 3,
+        username: "Rogerio Cezar",
+        login: "marcio1.cezar",
+        password: "secret@123"
+    }
+];
 
 export async function POST(request) {
     try {
@@ -22,8 +29,25 @@ export async function POST(request) {
         const body = await request.json();
         const { login, password } = body;
 
-        const teste = `Usuario: ${login} Senha: ${password}`
-        return NextResponse.json(teste);
+        const user = await mockUsers.find(u => u.login === login);
+        if( !user || user.password !== password){
+            return NextResponse.json(
+                { error: 'Usuário ou senha inválidos'},
+                { status: 401}
+            );
+        }
+        //const teste = `Usuario: ${login} Senha: ${password}`
+        const token = jwt.sign(
+            {
+                id: user.id,
+                username: user.username,
+                login : user.login
+            },
+            JWT_SECRET,
+            { expiresIn: '1h'}
+        );
+
+        return NextResponse.json({token});
     } catch (error) {
         //Bloco 2
         return NextResponse.json(
